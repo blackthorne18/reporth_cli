8fbfdfe2c0a09775d5d46bf3a6020fd28beceaa0
Prajwal Bharadwaj <prajwal17@iiserb.ac.in>
2023-12-24 02:35:53 +0000

    Updated readme and setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name='repinclusterer',
-    version='0.6.0',
+    version='0.7.0',
     author='Prajwal Bharadwaj',
     description='Cluster DNA Sequences based on orthology',
     long_description=long_description,
